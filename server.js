8df7ec8f3ab131d4b4e79984cd7b1181ee04f14f
lucas dalessandro <lucas.dalessandro@gmail.com>
2020-07-06 13:48:04 -0400

    1.1 + server

--- server.js
+++ server.js
@@ -1,27 +1,28 @@
-'use strict';
+"use strict";
 
-const morgan = require('morgan');
-
-const { top50 } = require('./data/top50');
+const morgan = require("morgan");
+const express = require("express");
+const { top50 } = require("./data/top50");
 
 const PORT = process.env.PORT || 8000;
 
 const app = express();
 
-app.use(morgan('dev'));
-app.use(express.static('public'));
-app.use(express.urlencoded({extended: false}));
-app.set('view engine', 'ejs');
+app.use(morgan("dev"));
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: false }));
+app.set("view engine", "ejs");
 
 // endpoints here
-
+const home = (req, res) => res.render("pages/top50", { title: "top 50" });
+app.get("/top50", home);
 // handle 404s
-app.git('*', (req, res) => {
-    res.status(404);
-    res.render('pages/fourOhFour', {
-        title: 'I got nothing',
-        path: req.originalUrl
-    });
+app.get("*", (req, res) => {
+  res.status(404);
+  res.render("pages/fourOhFour", {
+    title: "I got nothing",
+    path: req.originalUrl,
+  });
 });
 
-get.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
